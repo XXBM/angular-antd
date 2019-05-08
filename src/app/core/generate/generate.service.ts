16b959ed8f02323abb0a112176c1aa054af87cf9
hlh <17865312379@163.com>
2019-05-08 16:44:16 +0800

    穿梭框

--- src/app/core/generate/generate.service.ts
+++ src/app/core/generate/generate.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenerateService {
-
+  private getDomainNameWithTitleUrl = `/automation/getDomainNamesWithTitle`;
   constructor(private http: HttpClient) { }
 
+  /*获取所有实体类名*/
+  getDomainsWithTitle(): Observable<{}> {
+    return this.http.get(this.getDomainNameWithTitleUrl);
+  }
 
 }
--- src/app/tasks/generate/dao/dao.component.ts
+++ src/app/tasks/generate/dao/dao.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NzMessageService} from 'ng-zorro-antd';
+import {GenerateService} from '../../../core/generate/generate.service';
 
 @Component({
   selector: 'app-dao',
@@ -6,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dao.component.less']
 })
 export class DaoComponent implements OnInit {
+  /*穿梭框数据*/
+  domains: {};
+  /*穿梭时传的数据*/
+  daosing: {};
 
-  constructor() { }
+
+  constructor( private message: NzMessageService,
+               private generateService: GenerateService) { }
+
+  /*左框-所有实体类*/
+  getDomainWithTitle(): void {
+    this.generateService.getDomainsWithTitle().subscribe(
+      data => {
+        this.domains = data;
+      },
+      error => {
+        this.message.error('载入数据错误。');
+      }
+    );
+  }
 
   ngOnInit() {
+    this.getDomainWithTitle();
+  }
+
+  /*选中某一个时触发的方法*/
+  select(ret: {}): void {
+    console.log('nzSelectChange', ret);
+  }
+
+  /*穿梭时触发的方法*/
+  change(ret: {list}): void {
+    this.daosing = ret.list;
+    console.log(ret);
+    console.log( ret.list);
+    console.log( JSON.stringify(ret.list));
+
+  }
+
+  /*给父组件返回要生成的类名数组*/
+  rightFiles(): string[] {
+    const str = [];
+    // tslint:disable-next-line:forin
+    for (const item in this.daosing) {
+      str.push(this.daosing[item].title);
+    }
+    return str;
   }
 
 }
